0d2fcb9ffc7cd49902a3dd6c4826cf00df07214e
Guoli Lyu <guoli-lv@hotmail.com>
2018-08-02 16:55:21 +0800

    feat(connectivity): connectivity around corner

--- doc_scanner/math_utils.py
+++ doc_scanner/math_utils.py
@@ -47,3 +47,78 @@ def find_point_polar(line: pd.DataFrame, x: tuple):
     dist = line['intercept']
     y = tuple(map(lambda i: (dist - i * np.cos(angle)) / np.sin(angle), x))
     return y
+
+
+def interpolate_pixels_along_line(p1: pd.DataFrame, p2: pd.DataFrame):
+    """Uses Xiaolin Wu's line algorithm to interpolate all of the pixels along a
+    straight line, given two points (x0, y0) and (x1, y1)
+
+    Wikipedia article containing pseudo code that function was based off of:
+        http://en.wikipedia.org/wiki/Xiaolin_Wu's_line_algorithm
+
+    Given by Rick(https://stackoverflow.com/users/2025958/rick)
+    on https://stackoverflow.com/questions/24702868/python3-pillow-get-all-pixels-on-a-line.
+    """
+    (x1, y1) = p1.values.flatten()
+    (x2, y2) = p2.values.flatten()
+    pixels = []
+    steep = np.abs(y2 - y1) > np.abs(x2 - x1)
+
+    # Ensure that the path to be interpolated is shallow and from left to right
+    if steep:
+        t = x1
+        x1 = y1
+        y1 = t
+
+        t = x2
+        x2 = y2
+        y2 = t
+
+    if x1 > x2:
+        t = x1
+        x1 = x2
+        x2 = t
+
+        t = y1
+        y1 = y2
+        y2 = t
+
+    dx = x2 - x1
+    dy = y2 - y1
+    gradient = dy / dx  # slope
+
+    # Get the first given coordinate and add it to the return list
+    x_end = np.round(x1)
+    y_end = y1 + (gradient * (x_end - x1))
+    xpxl0 = x_end
+    ypxl0 = np.round(y_end)
+    if steep:
+        pixels.extend([(ypxl0, xpxl0), (ypxl0 + 1, xpxl0)])
+    else:
+        pixels.extend([(xpxl0, ypxl0), (xpxl0, ypxl0 + 1)])
+
+    interpolated_y = y_end + gradient
+
+    # Get the second given coordinate to give the main loop a range
+    x_end = np.round(x2)
+    y_end = y2 + (gradient * (x_end - x2))
+    xpxl1 = x_end
+    ypxl1 = np.round(y_end)
+
+    # Loop between the first x coordinate and the second x coordinate, interpolating the y coordinates
+    for x in np.arange(xpxl0 + 1, xpxl1):
+        if steep:
+            pixels.extend([(np.floor(interpolated_y), x), (np.floor(interpolated_y) + 1, x)])
+
+        else:
+            pixels.extend([(x, np.floor(interpolated_y)), (x, np.floor(interpolated_y) + 1)])
+
+        interpolated_y += gradient
+
+    # Add the second given coordinate to the given list
+    if steep:
+        pixels.extend([(ypxl1, xpxl1), (ypxl1 + 1, xpxl1)])
+    else:
+        pixels.extend([(xpxl1, ypxl1), (xpxl1, ypxl1 + 1)])
+
+    return pd.DataFrame(pixels, columns=['x', 'y'], dtype=int)
--- doc_scanner/scanner.py
+++ doc_scanner/scanner.py
@@ -1,17 +1,16 @@
 import cv2
-import numpy as np
 import pandas as pd
 import itertools
 from dataclasses import dataclass
 from skimage.transform import hough_line_peaks, hough_line
-from doc_scanner.math_utils import points2line, find_point_polar, intersection
+from doc_scanner.math_utils import points2line, find_point_polar, intersection, interpolate_pixels_along_line
 
 
 @dataclass
 class HoughResult:
-    h: np.array
-    theta: np.array
-    distance: np.array
+    h: pd.np.array
+    theta: pd.np.array
+    distance: pd.np.array
 
 
 @dataclass
@@ -22,14 +21,14 @@ class ProcessingResult:
     contours is opencv result of all contours
     hough is the result of line hough transformation in image-scikit
     """
-    blurred: np.array
-    edges: np.array
-    contour_image: np.array
-    hist_equalized: np.array
+    blurred: pd.np.array
+    edges: pd.np.array
+    contour_image: pd.np.array
+    hist_equalized: pd.np.array
 
-    hist: np.array
+    hist: pd.np.array
 
-    contours: np.array
+    contours: pd.np.array
 
     hough: HoughResult
 
@@ -62,7 +61,7 @@ def filter_and_edge_detect(image, kernel_size=15, intensity_lower=0, intensity_u
 
     # Morphological Open operation
     # Determine kernel size according to a priori knowledge on the size of words
-    kernel = np.ones((kernel_size, kernel_size), dtype=np.int8)
+    kernel = pd.np.ones((kernel_size, kernel_size), dtype=pd.np.int8)
     hist_equalized = cv2.morphologyEx(hist_equalized, cv2.MORPH_OPEN, kernel)
     hist_equalized = cv2.morphologyEx(hist_equalized, cv2.MORPH_CLOSE, kernel)
 
@@ -81,17 +80,17 @@ def filter_and_edge_detect(image, kernel_size=15, intensity_lower=0, intensity_u
     # TODO decide thresholds
     edges = cv2.Canny(filtered, canny_lower, canny_upper, L2gradient=True, apertureSize=3)
     _, contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contour_image = cv2.drawContours(np.zeros(image.shape[0:2]), contours, -1, (128, 255, 0), 3)
-    theta = np.linspace(-np.pi * 1 / 4, np.pi * 3 / 4, 180)
+    contour_image = cv2.drawContours(pd.np.zeros(image.shape[0:2]), contours, -1, (128, 255, 0), 3)
+    theta = pd.np.linspace(-pd.np.pi * 1 / 4, pd.np.pi * 3 / 4, 180)
     hough = hough_line(contour_image, theta)
     return ProcessingResult(blurred=blurred, edges=edges, contour_image=contour_image, hist_equalized=hist_equalized,
                             hist=hist, hough=HoughResult(*hough), contours=contours)
 
 
-def select_edge(result: ProcessingResult, ax=None, image: np.array = None):
+def select_edge(result: ProcessingResult, ax=None, image: pd.np.array = None):
     lines = hough_line_peaks(result.hough.h, result.hough.theta, result.hough.distance, min_distance=10, min_angle=50,
-                             threshold=0.5 * result.hough.h.max(), num_peaks=np.inf)
-    lines = pd.DataFrame(np.array(lines).T, columns=['hits', 'angle', 'intercept'])
+                             threshold=0.5 * result.hough.h.max(), num_peaks=pd.np.inf)
+    lines = pd.DataFrame(pd.np.array(lines).T, columns=['hits', 'angle', 'intercept'])
     _divide_line_orientation(lines)
     intersections = _find_intersections(lines, result.contour_image)
 
@@ -116,7 +115,7 @@ def select_edge(result: ProcessingResult, ax=None, image: np.array = None):
     return list(lines)
 
 
-def _divide_line_orientation(lines: pd.DataFrame, err=np.pi * 1 / 12, inplace: bool = True):
+def _divide_line_orientation(lines: pd.DataFrame, err=pd.np.pi * 1 / 12, inplace: bool = True):
     """
     Discriminate between horizontal and vertical lines
     :param lines: lines in polar coordination
@@ -132,7 +131,7 @@ def _divide_line_orientation(lines: pd.DataFrame, err=np.pi * 1 / 12, inplace: b
         if abs(line['angle']) < err:
             # vertical
             direction = 'vertical'
-        elif abs(line['angle'] - np.pi / 2) < err:
+        elif abs(line['angle'] - pd.np.pi / 2) < err:
             # horizontal
             direction = 'horizontal'
         else:
@@ -142,7 +141,7 @@ def _divide_line_orientation(lines: pd.DataFrame, err=np.pi * 1 / 12, inplace: b
     return out
 
 
-def _pairwise_intersection(horizontal_polar, vertical_polar, contour_image=None, along_length=50):
+def _intersection_connectivity(horizontal_polar, vertical_polar, contour_image, along_length=50):
     """ Compute connectivity given a horizontal line and vertical line in polar coordination.
     1. convert lines to cartesian coordination
     2. find intersection in cartesian coordination
@@ -155,88 +154,71 @@ def _pairwise_intersection(horizontal_polar, vertical_polar, contour_image=None,
     :return:
     """
 
-    if contour_image is not None:
-        x = (0, contour_image.shape[1])
-    else:
-        x = (0, 1000)
+    x = (0, contour_image.shape[1])
 
     y_h = find_point_polar(horizontal_polar, x)
     y_v = find_point_polar(vertical_polar, x)
 
-    horizontal_line_points = pd.DataFrame(list(zip(x, y_h)), columns=['x', 'y'])
-    vertical_line_points = pd.DataFrame(list(zip(x, y_v)), columns=['x', 'y'])
+    points_h = pd.DataFrame(list(zip(x, y_h)), columns=['x', 'y'], dtype=float)
+    points_v = pd.DataFrame(list(zip(x, y_v)), columns=['x', 'y'], dtype=float)
 
-    point = intersection(points2line(horizontal_line_points), points2line(vertical_line_points))
+    intersection_point = intersection(points2line(points_h), points2line(points_v))
 
-    return point
+    if points_h['x'].diff()[1] < 0:
+        points_h = points_h.iloc[::-1]
+    if points_v['y'].diff()[1] < 0:
+        points_v = points_v.iloc[::-1]
+    edge_points = pd.DataFrame(columns=['x', 'y'])
+    edge_points = edge_points.append(points_h, ignore_index=True)
+    edge_points = edge_points.append(points_v, ignore_index=True)
+    edge_points.index = pd.Index(['left', 'right', 'down', 'top'])
+
+    connectivity = dict()
+    for direction, point in edge_points.iterrows():
+        distance = pd.np.sqrt(
+            (point['y'] - intersection_point['y']) ** 2 + (point['x'] - intersection_point['x']) ** 2)[0]
+        if distance >= along_length:
+            ratio = along_length / distance
+            end = pd.np.round((1 - ratio) * intersection_point + ratio * point)
+            pixels = interpolate_pixels_along_line(intersection_point, end)
+            hits = 0
+            for ix, pixel in pixels.iterrows():
+                if contour_image[pixel['y'], pixel['x']] > 0:
+                    hits += 1
+            # TODO justify and improve connectivity criterion
+            connectivity[direction] = dict(hits=hits, pixel_along=len(pixels), connectivity=hits / len(pixels))
+    connectivity = pd.DataFrame(connectivity).T.sort_values(by='connectivity', ascending=False)
+
+    return intersection_point, connectivity
 
 
-def _find_intersections(lines, contour_image=None):
+def _corner_orientation(connectivity):
+    """determine the orientation of corner
+
+    :param connectivity:
+    :return:
+    """
+    top2_orientations = connectivity.iloc[0:2].index
+    if {'top', 'left'} == set(top2_orientations):
+        pass
+    elif {'top', 'right'} == set(top2_orientations):
+        pass
+    elif {'down', 'right'} == set(top2_orientations):
+        pass
+    elif {'down', 'right'} == set(top2_orientations):
+        pass
+
+
+def _find_intersections(lines, contour_image):
     try:
         lines['direction']
     except ValueError:
         _divide_line_orientation(lines)
     vertical_lines = lines[lines['direction'] == 'vertical']
     horizontal_lines = lines[lines['direction'] == 'horizontal']
-    combinations = itertools.product(vertical_lines.iterrows(), horizontal_lines.iterrows())
+    combinations = list(itertools.product(vertical_lines.iterrows(), horizontal_lines.iterrows()))
     intersections = pd.DataFrame()
     for (_, vertical_line), (_, horizontal_line) in combinations:
-        point = _pairwise_intersection(horizontal_line, vertical_line, contour_image)
+        point, connectivity = _intersection_connectivity(horizontal_line, vertical_line, contour_image)
         intersections = intersections.append(point)
     return intersections
-
-# class scanner:
-#     def __init__(self, image):
-#         """
-#         :param image: RGB
-#         """
-#         self.__image = image
-#
-#         # Convert RGB to HSV colorspace
-#         hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-#
-#         # hue ranges from 0-180
-#         self.hue = hsv[:, :, 0]
-#         self.saturation = hsv[:, :, 1]
-#         self.intensity = hsv[:, :, 2]
-#
-#     def __preprocess(self, img, blur_size=25, morphology_kernel_size=15, intensity_lower=0, intensity_upper=255):
-#         """Preprocess pipeline
-#         1. Blur
-#         1. Histogram equalization
-#         1. Morphological operation (Opening)
-#         1. (Optional) Threshold based segmentation.
-#
-#             Here we assume that the document of interest is mainly white while background is darker.
-#             Then we can extract document from background with a proper threshold.
-#             After histogram, maybe we can just assume the document lays in the half brighter part on histogram.
-#         :param img: 2D image (Hue or saturation or intensity)
-#         :param blur_size:
-#         :param morphology_kernel_size:
-#         :param intensity_lower:
-#         :param intensity_upper:
-#         :return:
-#         """
-#         # blurred = cv2.GaussianBlur(image, (5, 5), 0)
-#         # blurred = cv2.bilateralFilter(image, 9, 50, 50)
-#         blurred = cv2.medianBlur(img, blur_size)
-#         hist_equalized = cv2.equalizeHist(blurred)
-#
-#         # Morphological Open operation
-#         # Determine kernel size according to a priori knowledge on the size of words
-#         kernel = np.ones((morphology_kernel_size, morphology_kernel_size), dtype=np.int8)
-#         hist_equalized = cv2.morphologyEx(hist_equalized, cv2.MORPH_OPEN, kernel)
-#         hist_equalized = cv2.morphologyEx(hist_equalized, cv2.MORPH_CLOSE, kernel)
-#
-#         # hist = cv2.calcHist([hist_equalized], [0], None, [256], [0, 256])
-#         # plt.bar(np.arange(len(hist)), hist.flatten())
-#         # plt.show()
-#
-#         # TODO intensity threshold filter can bring artifacts
-#         # Threshold the intensity image or gray scale image
-#         # ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-#         mask = cv2.inRange(hist_equalized, intensity_lower, intensity_upper)
-#
-#         # Bitwise-AND mask and original image
-#         filtered = cv2.bitwise_and(hist_equalized, hist_equalized, mask=mask)
-#         return filtered
